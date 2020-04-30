59dc293523017ae235c8ecab0d5b4754ddbb3fc4
sebiik <sebiik@gmail.com>
2020-05-01 01:22:43 +0800

    WM8731 output working

--- axoloti/firmware/codec_WM8731.c
+++ axoloti/firmware/codec_WM8731.c
@@ -1,16 +1,16 @@
 // #include "codec.h"
 #include "codec_WM8731.h"
 
-const stm32_dma_stream_t* i2sdmaWM;
-static uint32_t i2stxdmamodeWM = 0;
+const stm32_dma_stream_t* i2s_dma_tx;
 
 static const I2CConfig i2cfgWM = {OPMODE_I2C, 100000, STD_DUTY_CYCLE, };
 
 #define I2S2_TX_DMA_CHANNEL STM32_DMA_GETCHANNEL(STM32_SPI_SPI2_TX_DMA_STREAM, STM32_SPI2_TX_DMA_CHN)
 
 static uint8_t txbufWM[2] __attribute__ ((section (".sram2")));
-static uint8_t rxbufWM[2] __attribute__ ((section (".sram2")));
+// static uint8_t rxbufWM[2] __attribute__ ((section (".sram2")));
 
+// extern void dma_i2s_interrupt(void *, uint32_t);
 
 void codec_WM8731_hw_init(void) {
 
@@ -19,26 +19,39 @@ void codec_WM8731_hw_init(void) {
 
 	i2cStart(&WM8731_I2C, &i2cfgWM);
 
-	// Reset the codec
 	codec_WM8731_hw_reset();
+
+	// Slave Mode, I2S Data Format, 32bit, 48k //TODO: maybe input works with 32bit?
+	// codec_WM8731_writeReg(WM8731_REG_INTERFACE, 0b000001110);
+
+	// Slave Mode, I2S Data Format, 24bit, 48k
+	codec_WM8731_writeReg(WM8731_REG_INTERFACE, 0b000001010);
+
+
+	// 256fs, 48khz in, 48khz out, MCLK/2
+	codec_WM8731_writeReg(WM8731_REG_SAMPLING,  0b001000000);
+
+	// 256fs, 48khz in, 48khz out, MCLK
+	// codec_WM8731_writeReg(WM8731_REG_SAMPLING,  0b000000000);
+
 
 	// Keep codec powered down initially
 	codec_WM8731_pwrCtl(0);
+	codec_WM8731_muteCtl(0);
 
-	// Slave Mode, I2S Data Format, 32bit, 48k //TODO set to 24bit?
-	codec_WM8731_writeReg(WM8731_REG_INTERFACE, 0b00001110);
-	// 12.288mhz, 256fs, 48khz in, 48khz out, MCLK/2 //TODO 24mhz?
-	codec_WM8731_writeReg(WM8731_REG_SAMPLING,  0b01000000);
+	codec_WM8731_activeCtl(1);
+
+	chThdSleepMilliseconds(5);
 
 	codec_WM8731_pwrCtl(1);
 
-	codec_WM8731_muteCtl(0);
-	codec_WM8731_volCtl(0b00010111);
+	codec_WM8731_lineInVolCtl(0b00010111);
+	codec_WM8731_headphoneVolCtl(0b001111001);
 }
 
 void codec_WM8731_hw_reset(void) {
 	codec_WM8731_writeReg(WM8731_REG_RESET, 0x00);
-	chThdSleepMilliseconds(10);
+	chThdSleepMilliseconds(1);
 }
 
 static void dma_i2s_interrupt(void* dat, uint32_t flags) {
@@ -46,49 +59,51 @@ static void dma_i2s_interrupt(void* dat, uint32_t flags) {
   (void)dat;
   (void)flags;
 
-  if ((i2sdmaWM)->stream->CR & STM32_DMA_CR_CT)
+  if ((i2s_dma_tx)->stream->CR & STM32_DMA_CR_CT)
     computebufI(rbuf, buf);
   else
     computebufI(rbuf2, buf2);
 
-  dmaStreamClearInterrupt(i2sdmaWM);
+  dmaStreamClearInterrupt(i2s_dma_tx);
 }
 
 static void codec_WM8731_dma_init(void) {
 
-	i2sdmaWM = STM32_DMA_STREAM(STM32_SPI_SPI2_TX_DMA_STREAM);
+	i2s_dma_tx = STM32_DMA_STREAM(STM32_SPI_SPI2_TX_DMA_STREAM);
 
-	i2stxdmamodeWM = STM32_DMA_CR_CHSEL(I2S2_TX_DMA_CHANNEL) | STM32_DMA_CR_PL(STM32_SPI_SPI2_DMA_PRIORITY) | STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_TEIE | STM32_DMA_CR_TCIE | STM32_DMA_CR_DBM | /* double buffer mode */ STM32_DMA_CR_PSIZE_HWORD | STM32_DMA_CR_MSIZE_WORD;
+	uint32_t i2s_dma_tx_mode = STM32_DMA_CR_CHSEL(I2S2_TX_DMA_CHANNEL) | STM32_DMA_CR_PL(STM32_SPI_SPI2_DMA_PRIORITY) | STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_TEIE | STM32_DMA_CR_TCIE | STM32_DMA_CR_DBM | /* double buffer mode */ STM32_DMA_CR_PSIZE_HWORD | STM32_DMA_CR_MSIZE_WORD;
 
-	dmaStreamAllocate(i2sdmaWM, STM32_SPI_SPI2_IRQ_PRIORITY, (stm32_dmaisr_t)dma_i2s_interrupt, (void *)&SPID2);
+	dmaStreamAllocate(i2s_dma_tx, STM32_SPI_SPI2_IRQ_PRIORITY, (stm32_dmaisr_t)dma_i2s_interrupt, (void *)&SPID2);
 
-	dmaStreamSetPeripheral(i2sdmaWM, &(WM8731_I2S->DR));
-	dmaStreamSetMemory0(i2sdmaWM, buf);
-	dmaStreamSetMemory1(i2sdmaWM, buf2);
-	dmaStreamSetTransactionSize(i2sdmaWM, 64);
-	dmaStreamSetMode(i2sdmaWM, i2stxdmamodeWM | STM32_DMA_CR_MINC);
+	dmaStreamSetPeripheral(i2s_dma_tx, &(WM8731_I2S->DR));
+	dmaStreamSetMemory0(i2s_dma_tx, buf);
+	dmaStreamSetMemory1(i2s_dma_tx, buf2);
+	dmaStreamSetTransactionSize(i2s_dma_tx, 64);
+	dmaStreamSetMode(i2s_dma_tx, i2s_dma_tx_mode | STM32_DMA_CR_MINC);
 
 	//TODO: add RX
 
-	dmaStreamClearInterrupt(i2sdmaWM);
-	dmaStreamEnable(i2sdmaWM);
+	dmaStreamClearInterrupt(i2s_dma_tx);
+	dmaStreamEnable(i2s_dma_tx);
 }
 
 void codec_WM8731_i2s_init_48k(void) {
 
 	//TODO: | PAL_STM32_OSPEED_HIGHEST?
-	//TODO: palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL);
+	// ..._PUSHPULL is what fixed it!
+	palSetPadMode(GPIOB, 12, PAL_MODE_OUTPUT_PUSHPULL);
 	palSetPadMode(GPIOB, 12, PAL_MODE_ALTERNATE(5)); //WS
 
-	// palSetPadMode(GPIOB, 13, PAL_MODE_OUTPUT_PUSHPULL);
+	palSetPadMode(GPIOB, 13, PAL_MODE_OUTPUT_PUSHPULL);
 	palSetPadMode(GPIOB, 13, PAL_MODE_ALTERNATE(5)); //BCK
 
+	// palSetPadMode(GPIOB, 14, PAL_MODE_INPUT); //TODO try this
 	palSetPadMode(GPIOB, 14, PAL_MODE_ALTERNATE(5)); //ext_SD maybe AF6?
 
-	// palSetPadMode(GPIOB, 15, PAL_MODE_OUTPUT_PUSHPULL);
+	palSetPadMode(GPIOB, 15, PAL_MODE_OUTPUT_PUSHPULL);
 	palSetPadMode(GPIOB, 15, PAL_MODE_ALTERNATE(5)); //SD
 
-	// palSetPadMode(GPIOC, 6, PAL_MODE_OUTPUT_PUSHPULL);
+	palSetPadMode(GPIOC, 6, PAL_MODE_OUTPUT_PUSHPULL);
 	palSetPadMode(GPIOC, 6,  PAL_MODE_ALTERNATE(5)); //MCLK
 
 // SPI2 in I2S Mode, Master
@@ -101,11 +116,11 @@ void codec_WM8731_i2s_init_48k(void) {
 	WM8731_I2S->I2SCFGR |= SPI_I2SCFGR_I2SE;
 }
 
-void codec_WM8731_writeReg(uint8_t addr, uint8_t data) {
+void codec_WM8731_writeReg(uint8_t addr, uint16_t data) {
 
 	txbufWM[0] = (addr << 1) | ((data >> 8) & 1);
 	txbufWM[1] = data & 0xFF;
-	
+
 	i2cMasterTransmitTimeout(&WM8731_I2C, WM8731_I2C_ADDR, txbufWM, 2, NULL, 0, MS2ST(4));
   chThdSleepMilliseconds(1);
 }
@@ -120,15 +135,16 @@ void codec_WM8731_pwrCtl(uint8_t pwr) {
 
 void codec_WM8731_muteCtl(uint8_t mute) {
 
-	if (mute)
+	if (mute) {
 		codec_WM8731_writeReg(WM8731_REG_DIGITAL, 0x08);
+  }
 	else {
 		codec_WM8731_writeReg(WM8731_REG_DIGITAL, 0x00);
-		codec_WM8731_writeReg(WM8731_REG_ANALOG, 0x10);    // DAC selected
+		codec_WM8731_writeReg(WM8731_REG_ANALOG, 0x12);    // DAC selected, mute mic
 	}
 }
 
-void codec_WM8731_volCtl(uint8_t vol) {
+void codec_WM8731_lineInVolCtl(uint8_t vol) {
 
 	if (vol <= 0x1F) {
 		codec_WM8731_writeReg(WM8731_REG_LLINEIN, vol & 0x1F);
@@ -138,4 +154,24 @@ void codec_WM8731_volCtl(uint8_t vol) {
 		codec_WM8731_writeReg(WM8731_REG_LLINEIN, 0x1F);
 		codec_WM8731_writeReg(WM8731_REG_RLINEIN, 0x1F);
 	}
+}
+
+void codec_WM8731_headphoneVolCtl(uint16_t vol) {
+
+	if (vol <= 0x7F) {
+		codec_WM8731_writeReg(WM8731_REG_LHEADOUT, vol & 0x7F);
+		codec_WM8731_writeReg(WM8731_REG_RHEADOUT, vol & 0x7F);
+	}
+	else {
+		codec_WM8731_writeReg(WM8731_REG_LHEADOUT, 0x7F);
+		codec_WM8731_writeReg(WM8731_REG_RHEADOUT, 0x7F);
+	}
+}
+
+void codec_WM8731_activeCtl(uint8_t active) {
+
+	if (active)
+		codec_WM8731_writeReg(WM8731_REG_ACTIVE, 0x01);
+	else
+		codec_WM8731_writeReg(WM8731_REG_ACTIVE, 0x00);
 }
--- axoloti/firmware/codec_WM8731.h
+++ axoloti/firmware/codec_WM8731.h
@@ -4,31 +4,33 @@
 #include "ch.h"
 #include "hal.h"
 
-#define WM8731_I2C I2CD2
 #define WM8731_I2S_ENABLE rccEnableSPI2(FALSE)
 #define WM8731_I2S_DISABLE rccDisableSPI2(FALSE)
+#define WM8731_I2C I2CD2
 #define WM8731_I2S SPI2
-#define WM8731_I2Sext I2S2ext
+#define WM8731_I2SEXT I2S2ext
 
-#define WM8731_I2C_ADDR 0x1A // address when CSB pin to ground, else 0x1B
-#define WM8731_REG_LLINEIN  	0 // 0b 000 0000
-#define WM8731_REG_RLINEIN  	1 // 0b 000 0001
-#define WM8731_REG_LHEADOUT 	2 // 0b 000 0010
-#define WM8731_REG_RHEADOUT 	3 // 0b 000 0011
-#define WM8731_REG_ANALOG   	4 // 0b 000 0100
-#define WM8731_REG_DIGITAL	  5 // 0b 000 0101
-#define WM8731_REG_POWERDOWN  6 // 0b 000 0110
-#define WM8731_REG_INTERFACE	7 // 0b 000 0111
-#define WM8731_REG_SAMPLING 	8 // 0b 000 1000
-#define WM8731_REG_ACTIVE   	9 // 0b 000 1001
-#define WM8731_REG_RESET     15 // 0b 000 1111
+#define WM8731_I2C_ADDR    0x1A // 0x1A when CSB pin to ground, else 0x1B
+#define WM8731_REG_LLINEIN  	0
+#define WM8731_REG_RLINEIN  	1
+#define WM8731_REG_LHEADOUT 	2
+#define WM8731_REG_RHEADOUT 	3
+#define WM8731_REG_ANALOG   	4
+#define WM8731_REG_DIGITAL	  5
+#define WM8731_REG_POWERDOWN  6
+#define WM8731_REG_INTERFACE	7
+#define WM8731_REG_SAMPLING 	8
+#define WM8731_REG_ACTIVE   	9
+#define WM8731_REG_RESET     15
 
 extern void codec_WM8731_hw_init(void);
 extern void codec_WM8731_hw_reset(void);
-extern void codec_WM8731_writeReg(uint8_t addr, uint8_t data);
-extern void codec_WM8731_volCtl(uint8_t vol);
+extern void codec_WM8731_writeReg(uint8_t addr, uint16_t data);
+extern void codec_WM8731_lineInVolCtl(uint8_t vol);
+extern void codec_WM8731_headphoneVolCtl(uint16_t vol);
 extern void codec_WM8731_pwrCtl(uint8_t pwr);
 extern void codec_WM8731_muteCtl(uint8_t mute);
+extern void codec_WM8731_activeCtl(uint8_t active);
 extern void codec_WM8731_i2s_init_48k(void);
 extern void codec_WM8721_I2CStop(void);
 
